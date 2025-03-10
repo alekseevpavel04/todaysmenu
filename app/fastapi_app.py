dd529fb6f2d9b684f5159f726d58a8139acdd244
alekseevpavel04 <alekceevpavel@mail.ru>
2025-03-10 20:05:38 +0300

    add docker files

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,1 +1,13 @@
-#TODO
+# Сервис должен быть LLM моделью - deepseek самая легкая (языковая)
+# У нее должна быть роль - Повар
+# Она будет принимать запросы пользователя что он хочет приготовить и выдавать рецепты
+
+
+os.environ["PYTHONWARNINGS"] = "ignore"
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# uvicorn fastapi_app:app --reload
